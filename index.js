8eca41f572688a53e3dc63c0f0522d8eaa74363c
serenna1 <rotondaserena3@gmail.com>
2025-09-09 19:38:43 -0300

    mejorado

--- index.js
+++ index.js
@@ -2,16 +2,42 @@ import { preguntas } from "./preguntas.js";
 import chalk from "chalk";
 import promptSync from "prompt-sync";
 
-//hay que mejorarlo, y quiero que haya mas funciones no una sola
-
 function reglas() {
   console.log(
     chalk.blueBright(`El Quiz es un multiple choice
-                      para marcar sus respuestas debera ingresar un numero del 0 al 3
+                      para marcar sus respuestas debera ingresar un numero del 1 al 4
                           (si desea finalizar el quiz ingrese "fin")`)
   );
 }
-
+function mostrarPreguntas(posicion) {
+  console.log(chalk.blackBright(preguntas[posicion].pregunta));
+}
+function mostrarPosiblesRespuestas(posicion) {
+  console.log(
+    chalk.magenta(`las posibles respuestas:
+            1.${preguntas[posicion].respuestas[0]}.
+            2.${preguntas[posicion].respuestas[1]}. 
+            3.${preguntas[posicion].respuestas[2]}.
+            4.${preguntas[posicion].respuestas[3]}`)
+  );
+}
+function ingreseRespuesta() {
+  while (true) {
+    const entrada = prompt(
+      chalk.blueBright(
+        `Ingrese su respuesta (1-4), si es correcta se sumaran 5 puntos: `
+      )
+    );
+    if (entrada === "fin") {
+      return "fin";
+    } else if (["1", "2", "3", "4"].includes(entrada)) {
+      //[1,2,3,4].incluides(usuairio) => de vuelve true si usuario tiene alguno de los valores si esta negado devuelve false
+      return Number(entrada);
+    } else {
+      console.log("Valor invalido. Ingrese 1-4 o `fin`");
+    }
+  }
+}
 function finalizarJuego(contadorDeError, contadorDePuntaje) {
   console.log(`Enhorabuena ha finalizado su Quiz, su total de puntos es ${contadorDePuntaje}
           La cantidad de respuestas incorrectas: ${contadorDeError}.`);
@@ -20,37 +46,21 @@ function finalizarJuego(contadorDeError, contadorDePuntaje) {
 function estructuraDelJuego(contadorDeError, contadorDePuntaje) {
   let usuario;
   for (let i = 0; i < preguntas.length; i++) {
-    console.log(chalk.blackBright(preguntas[i].pregunta));
-    console.log(
-      chalk.magenta(`las posibles respuestas:
-            1.${preguntas[i].respuestas[0]}.
-            2.${preguntas[i].respuestas[1]}. 
-            3.${preguntas[i].respuestas[2]}.
-            4.${preguntas[i].respuestas[3]}`)
-    );
-    usuario = Number(
-      prompt(
-        chalk.blueBright(
-          `Ingrese su respuesta, si es correcta se sumaran 5 puntos: `
-        )
-      )
-    );
-    if (usuario === 100) {
-      // arreglar el 100 por el fin
+    mostrarPreguntas(i);
+    mostrarPosiblesRespuestas(i);
+
+    usuario = ingreseRespuesta();
+
+    if (usuario === "fin") {
       return finalizarJuego(contadorDeError, contadorDePuntaje);
     }
-    if (
-      usuario !== 1 &&
-      usuario !== 2 &&
-      usuario !== 3 &&
-      usuario !== 4 &&
-      usuario !== 100
-    ) {
-      // buscar una forma para qu esea mas eficiente
+
+    usuario = Number(usuario);
+    if (![1, 2, 3, 4].includes(usuario)) {
       console.log(
         `Ingreso un valor incorrecto.
-        recuerde debe ingresar 1,2,3,4 o si desea finalizar 100
-        vuelva a intentarlo`
+         recuerde debe ingresar 1,2,3,4 o si desea finalizar ingrese "fin"
+         vuelva a intentarlo`
       );
     }
     if (usuario - 1 === preguntas[i].correcta) {
